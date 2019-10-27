9fdc5ec533f2c2de659135304d94139dd198ee16
SvitlanaChala <56609654+SvitlanaChala@users.noreply.github.com>
2019-10-27 18:41:03 +0200

    Delete 5.js

--- 5.js
+++
@@ -1,9 +0,0 @@
-const data = '235a125346a34asgas129ga';
- let result = '';
-
- for (let i = 0; i < data.length; i++) {
-   if (data[i] % 2 ===0 ) {
-     result += data[i];
-   }
- }
-console.log(result);
